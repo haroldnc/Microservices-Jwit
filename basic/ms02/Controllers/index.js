f17f06ddb90dcf99378678a2fe6165247650e4cd
Harold Navarro <harold.mth95@gmail.com>
2022-06-17 11:52:21 -0500

    Controlamos el error en el controlador PersonalData

--- basic/ms02/Controllers/index.js
+++ basic/ms02/Controllers/index.js
@@ -1,14 +1,23 @@
 function PersonalData ({ info }) {
-	const editors = {
-		vscode: 'microsoft',
-		vim: 'Bram Moolenaar',
-		'sublime text': 'Jon Skinner',
-		'notepad++': 'Don Ho'
+	try {
+		const editors = {
+			vscode: 'microsoft',
+			vim: 'Bram Moolenaar',
+			'sublime text': 'Jon Skinner',
+			'notepad++': 'Don Ho'
+		}
+
+		console.log(`The ${info.editor} editor developed by ${editors[info.editor]}`)
+
+		return info;
+	} catch(error) {
+		console.log({
+			step: 'controllers PersonalData',
+			error: error.toString()
+		});
+
+		return { statusCode:500, message: error.toString() };
 	}
-
-	console.log(`The ${info.editor} editor developed by ${editors[info.editor]}`)
-
-	return info;
 }
 
 function PreferencesColor ({ color }) {
