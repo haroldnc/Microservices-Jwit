613977ccdd5953725f1670ddb2d9df6bf8f11242
Harold Navarro <harold.mth95@gmail.com>
2022-06-17 11:40:35 -0500

    Manejamos el error en la petición al Adaptador

--- basic/ms02/index.js
+++ basic/ms02/index.js
@@ -1,14 +1,22 @@
 const { Adaptador } = require('./Adapters');
 
-const result = Adaptador({
-	info: {
-		name: 'Jack',
-		edad: 27,
-		address: 'Calle Libertad, Chocope, Perú',
-		nationality: 'peruano',
-		editor: 'vim'
-	},
-	color: 'rojo'
-});
+const main = () => {
+	try {
+		const result = Adaptador({
+			info: {
+				name: 'Jack',
+				edad: 27,
+				address: 'Calle Libertad, Chocope, Perú',
+				nationality: 'peruano',
+				editor: 'vim'
+			},
+			color: 'rojo'
+		});
 
-console.log(result);
+		console.log(result);
+	} catch(error) {
+		console.error(error);
+	}
+}
+
+main();
