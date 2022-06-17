ce59169ab7fdf9910f0f5e303659cd171345fc08
Harold Navarro <harold.mth95@gmail.com>
2022-06-17 03:37:21 -0500

    Manejamos el error para validar la 'edad' mayor a 18 años

--- basic/ms01/homework/Services/index.js
+++ basic/ms01/homework/Services/index.js
@@ -4,6 +4,8 @@ const {
 } = require('../Controllers');
 
 function Servicio ({ info, color }) {
+	if (info.edad <= 18) throw new Error('The user is not older than 18 years old');
+
 	const personalData = PersonalData({ info });
 	const preferencesColor = PreferencesColor({ color });
 
