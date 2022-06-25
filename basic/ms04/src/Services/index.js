ccf8934ca87f64787b6ef58edbe24f70974a2728
Harold Navarro <harold.mth95@gmail.com>
2022-06-24 23:33:12 -0500

    Creamos el servicio View

--- basic/ms04/src/Services/index.js
+++ basic/ms04/src/Services/index.js
@@ -70,9 +70,34 @@ async function FindOne({ name, age, address, nationality, color, id }) {
 	}
 }
 
+async function View({ name, age, address, nationality, color, id }) {
+	try {
+		const { statusCode, data, message } = await Controllers.View({
+			where: {
+				name,
+				age,
+				address,
+				nationality,
+				color,
+				id
+			}
+		});
+
+		return { statusCode, data, message };
+	} catch(error) {
+		console.log({
+			step: 'services Create',
+			error: error.toString()
+		});
+
+		return { statusCode:500, message: error.toString() };
+	}
+}
+
 module.exports = {
 	Create,
 	Delete,
 	Update,
-	FindOne
+	FindOne,
+	View
 };
