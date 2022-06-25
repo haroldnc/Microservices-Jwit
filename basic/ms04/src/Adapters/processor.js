18bc9be14d253ef30da4d6ba6aa4abcb4e37fb04
Harold Navarro <harold.mth95@gmail.com>
2022-06-24 23:46:59 -0500

    Creamos las rutas y el procesador para Delete

--- basic/ms04/src/Adapters/processor.js
+++ basic/ms04/src/Adapters/processor.js
@@ -32,6 +32,23 @@ queueCreate.process(async (job, done) => {
 	}
 });
 
+queueDelete.process(async (job, done) => {
+	const { id } = job.data;
+
+	try {
+		const { statusCode, data, message } = await Services.Delete({ id });
+
+		done(null, { statusCode, data, message });
+	} catch(error) {
+		console.log({
+			step: 'adapters queueView',
+			error: error.toString()
+		});
+
+		done(null, { statusCode:500, message: InternalError });
+	}
+});
+
 queueView.process(async (job, done) => {
 	const { name, age, address, nationality, color, id } = job.data;
 
