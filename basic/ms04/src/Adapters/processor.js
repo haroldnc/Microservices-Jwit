fabccf22d486ce26a6746162b0550e7ae1d9a22f
Harold Navarro <harold.mth95@gmail.com>
2022-06-24 23:48:36 -0500

    Creamos las rutas y el procesador para Update

--- basic/ms04/src/Adapters/processor.js
+++ basic/ms04/src/Adapters/processor.js
@@ -49,6 +49,30 @@ queueDelete.process(async (job, done) => {
 	}
 });
 
+queueUpdate.process(async (job, done) => {
+	const { name, age, address, nationality, color, id } = job.data;
+
+	try {
+		const { statusCode, data, message } = await Services.Update({
+			name,
+			age,
+			address,
+			nationality,
+			color,
+			id
+		});
+
+		done(null, { statusCode, data, message });
+	} catch(error) {
+		console.log({
+			step: 'adapters queueView',
+			error: error.toString()
+		});
+
+		done(null, { statusCode:500, message: InternalError });
+	}
+});
+
 queueView.process(async (job, done) => {
 	const { name, age, address, nationality, color, id } = job.data;
 
