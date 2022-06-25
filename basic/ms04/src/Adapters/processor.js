ec33b841994c667d60b4a0f35b0af9e8e6e33720
Harold Navarro <harold.mth95@gmail.com>
2022-06-24 23:45:32 -0500

    Creamos las rutas y el procesador para Create

--- basic/ms04/src/Adapters/processor.js
+++ basic/ms04/src/Adapters/processor.js
@@ -8,6 +8,30 @@ const {
 	queueView
 } = require('./index');
 
+queueCreate.process(async (job, done) => {
+	const { name, age, address, nationality, color } = job.data;
+
+	try {
+		const { statusCode, data, message } = await Services.Create({
+			name,
+			age,
+			address,
+			nationality,
+			color,
+			id
+		});
+
+		done(null, { statusCode, data, message });
+	} catch(error) {
+		console.log({
+			step: 'adapters queueCreate',
+			error: error.toString()
+		});
+
+		done(null, { statusCode:500, message: InternalError });
+	}
+});
+
 queueView.process(async (job, done) => {
 	const { name, age, address, nationality, color, id } = job.data;
 
